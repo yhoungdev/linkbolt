af045c6a32869e1d394797387c879b570abca06f
yhoungdev <yhoungdev@gmail.com>
2023-07-14 23:49:08 +0100

    bug: fixed database relationship

--- server/controller/user_links_controller.ts
+++ server/controller/user_links_controller.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from "express";
 import * as Yup from "yup";
 import { StatusCode } from "../enums/staus_code";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 
 const router = express.Router();
+const prisma = new PrismaClient(); 
 
 const validation_schema = Yup.object().shape({
 	url: Yup.string().required().url(),
+	name: Yup.string().required("Platform name is required"), 
 });
 
 const validate_share_links = Yup.object().shape({
@@ -14,11 +17,25 @@ const validate_share_links = Yup.object().shape({
 	body: Yup.string().required()
 })
 
+
 export const save_user_link = async (req: Request, res: Response) => {
 	const body = req.body;
 	try {
 		const checks = await validation_schema.validate(body);
-		return res.status(StatusCode.OK).json({ success: checks.url });
+		const add_link = await prisma.links.create({
+			data: {
+				...checks, 
+				userId: body.userId
+			}, 
+			
+		})
+
+		if(add_link) {
+			return res.status(StatusCode.OK).json({ success: "Link successfully added" });
+		} else {
+			return res.status(401).send("something went wrong ")
+		}
+
 	} catch (err: any) {
 		return res.status(StatusCode.BadRequest).json({ error: err.message });
 	}
@@ -26,6 +43,7 @@ export const save_user_link = async (req: Request, res: Response) => {
 
 //share links to friends
 export const share_links = async  (req:Request, res: Response) => {
+
 	try {
 		const validateObj = await validate_share_links.validate(req.body);
 		return res.status(StatusCode.OK).json({success: validateObj})
@@ -34,3 +52,21 @@ export const share_links = async  (req:Request, res: Response) => {
 	}
 
 };
+
+
+
+//get links from user 
+export const get_user_links = async (req: Request , res: Response ) => {
+	const userId = req.params.id; 
+	try {
+		const get_links = await prisma.links.findMany({
+			where: {
+				userId: userId,
+			}
+		}); 
+		return res.status(StatusCode.OK).json({data: get_links}); 
+	} catch (err) {
+		res.status(StatusCode.OK).json({message: "No link found"});
+	}
+
+}
