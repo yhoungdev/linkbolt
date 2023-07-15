c16a954dfd364793ac84e31c8ff56e7d54a551e9
yhoungdev <yhoungdev@gmail.com>
2023-07-15 21:59:33 +0100

    feat: added signin with google call to action

---
+++ auth/src/components/molecules/header/header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div>hello</div>
+
+        <div className="bg-secondary ring-1  ring-primary h-[50px] w-[50px] rounded-full "></div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
